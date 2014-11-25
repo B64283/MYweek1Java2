d370c1f7de1226f25f330abcc19697a36a13f8f9
B64283 <mattdarke@fullsail.edu>
2014-11-25 10:59:47 -0800

    Define  master and detail fragments in layout

--- app/src/main/java/com/example/matthewdarke/myweek1java2/Fragments/DetailFragment.java
+++ app/src/main/java/com/example/matthewdarke/myweek1java2/Fragments/DetailFragment.java
@@ -1,6 +1,11 @@
 package com.example.matthewdarke.myweek1java2.Fragments;
-
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.matthewdarke.myweek1java2.R;
 
 /**
  * Created by matthewdarke on 11/25/14.
@@ -10,7 +15,21 @@ public class DetailFragment extends Fragment {
 
     //no  arguments constructor
     public DetailFragment() {
-        
 
+
+    }
+
+
+    //on create is automaticly called by framework inflates layout and returns resulting view
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.detail_fragment, container, false);
+
+       //returns root view when requested... Now when the instance of this fragment class is added to
+       //an activity, the oncall method will be called then it will create the view and return it and display it to the screen
+        return rootView;
     }
 }
--- app/src/main/java/com/example/matthewdarke/myweek1java2/Fragments/MasterFragment.java
+++ app/src/main/java/com/example/matthewdarke/myweek1java2/Fragments/MasterFragment.java
@@ -1,9 +1,35 @@
 package com.example.matthewdarke.myweek1java2.Fragments;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.matthewdarke.myweek1java2.R;
 
 /**
  * Created by matthewdarke on 11/25/14.
  */
 public class MasterFragment extends Fragment {
+
+
+  //no arguments constructor
+    public MasterFragment (){
+
+    }
+
+    //on create is automaticly called by framework inflates layout and returns resulting view
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.master_fragment, container, false);
+
+        //returns root view when requested... Now when the instance of this fragment class is added to
+        //an activity, the oncall method will be called then it will create the view and return it and display it to the screen
+        return rootView;
+    }
 }
+
+
